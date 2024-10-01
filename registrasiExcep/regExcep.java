42603aacacb7f19fda3723a224562f67132fd8d3
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-10-01 16:30:18 +0700

    Add files via upload

---
+++ registrasiExcep/regExcep.java
@@ -0,0 +1,49 @@
+package pertemuan5.registrasiExcep;
+import java.util.Scanner;
+
+public class regExcep {
+    static class InvalidNameException extends Exception {
+        public InvalidNameException(String message) {
+            super(message);
+        }
+    }
+
+
+    static void validateName(String name) throws InvalidNameException{
+        if(name.isEmpty()){
+            throw new InvalidNameException("Name cannot be blank");
+        }
+
+        for(char c:name.toCharArray()){
+            if(!Character.isLetter(c)){
+                throw new InvalidNameException("Name only can be written with letter");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String name;
+        int age;
+
+        try {
+            System.out.print("Enter your name: "+name);
+            name=scanner.nextLine();
+            validateName(name);
+            System.out.print("Age: "+age);
+            age=scanner.nextInt();
+
+            if(age<0){
+                throw new IllegalArgumentException("Age cannot be negative");
+            }
+
+            System.out.println("Registration succesfull --> "+name+", Age: "+age);
+        } catch (InvalidNameException ex) {
+            System.out.println("Error --> "+ex.getMessage());
+        } catch(IllegalArgumentException ex){
+            System.out.println("Error --> "+ex);
+        }finally{
+            scanner.close();
+        }
+    }
+}
